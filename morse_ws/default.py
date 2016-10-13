865631bf15a8c2c018f52d97b27dbbe45f965b27
ejalaa12 <ejalaa12@gmail.com>
2016-10-13 16:24:07 +0200

    Added the camera sensor

--- morse_ws/default.py
+++ morse_ws/default.py
@@ -41,20 +41,20 @@ keyboard.properties(Speed=4, ControlType='Position')
 #
 # #############################################################################
 
-# Pose
+# Pose ------------------------------------------------------------------------
 pose = Pose()
 robot.append(pose)
 pose.add_stream('ros')
 
-# Hokuyo Sensor
+# Hokuyo Sensor ---------------------------------------------------------------
+hokuyo = Hokuyo()
+hokuyo.translate(0, 0, 1)
+hokuyo.rotate(0, 0, pi / 2)
+# Properties
 # (https://www.hokuyo-aut.jp/02sensor/07scanner/urg_04lx_ug01.html)
 # 2cm to 5.6m | 240d |
 # Accuracy ±30mm (6cm<o<1m)
 # Accuracy 0.3% (1m<o<4.095m)
-hokuyo = Hokuyo()
-hokuyo.translate(0, 0, 1)
-hokuyo.rotate(0, 0, pi / 2)
-
 hokuyo.properties(laser_range=5.6)
 hokuyo.properties(resolution=0.36)
 hokuyo.properties(scan_window=240.0)
@@ -63,28 +63,46 @@ hokuyo.properties(scan_window=240.0)
 hokuyo.add_stream('ros')
 robot.append(hokuyo)
 
-# # Imu
+# Imu -------------------------------------------------------------------------
 imu = IMU()
 imu.translate(0, 0, 0)
 imu.rotate(0, 0, 0)
 imu.add_stream('ros')
 robot.append(imu)
 
+# Camera ----------------------------------------------------------------------
+videocamera = VideoCamera()
+videocamera.translate(0, 0, 1.5)
+videocamera.rotate(pi / 2, 0., 0.)
+# Properties
+videocamera.properties(cam_width=256, cam_height=256)
+videocamera.add_stream('ros')
+robot.append(videocamera)
 
+
+# #############################################################################
+#
+# For debugging ?
+#
+# #############################################################################
 # To ease development and debugging, we add a socket interface to our robot.
 #
 # Check here: http://www.openrobots.org/morse/doc/stable/user/integration.html
 # the other available interfaces (like ROS, YARP...)
 robot.add_default_interface('socket')
 
-
-# set 'fastmode' to True to switch to wireframe mode
+# #############################################################################
+#
+# Environment
+#
+# #############################################################################
+# PATH
 project_path = '/home/ejalaa12/Projects/GuerledanDamScanning/'
 env_folder = 'morse_ws/environments/'
 file = 'outdoors_flat_empty.blend'
+# ENVIRONMENT
 env = Environment(project_path + env_folder + file, fastmode=False)
 env.set_camera_location([-36.0, -15, 30])
 env.set_camera_rotation([1.09, 0, -1.14])
-
 # Properties for magnetomer in IMU
 env.properties(longitude=1.26, latitude=43.26, altitude=130.0)
