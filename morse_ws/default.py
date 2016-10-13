282f11b4504c02abca62892266de89757852596b
ejalaa12 <ejalaa12@gmail.com>
2016-10-13 16:11:41 +0200

    - Added two environments - Configured the simulated robot to have the hokuyo, GPS and IMU sensors

--- morse_ws/default.py
+++ morse_ws/default.py
@@ -4,55 +4,87 @@
 
 Feel free to edit this template as you like!
 """
-
 from morse.builder import *
+from math import pi
 
-# Add the MORSE mascott, MORSY.
-# Out-the-box available robots are listed here:
-# http://www.openrobots.org/morse/doc/stable/components_library.html
+# #############################################################################
 #
-# 'morse add robot <name> morse_ws' can help you to build custom robots.
-robot = Morsy()
-
-# The list of the main methods to manipulate your components
-# is here: http://www.openrobots.org/morse/doc/stable/user/builder_overview.html
-robot.translate(1.0, 0.0, 0.0)
-robot.rotate(0.0, 0.0, 3.5)
-
-# Add a motion controller
-# Check here the other available actuators:
-# http://www.openrobots.org/morse/doc/stable/components_library.html#actuators
+# ROBOT
 #
-# 'morse add actuator <name> morse_ws' can help you with the creation of a custom
-# actuator.
+# #############################################################################
+robot = ATRV()
+robot.translate(1.0, 0.0, 3.0)
+robot.rotate(0.0, 0.0, 0.)
+
+# #############################################################################
+#
+# Motion Controller
+#
+# #############################################################################
 motion = MotionVW()
+motion.add_stream('ros')
 robot.append(motion)
 
 
-# Add a keyboard controller to move the robot with arrow keys.
+# #############################################################################
+#
+# Keyboard Controller
+#
+# #############################################################################
 keyboard = Keyboard()
 robot.append(keyboard)
-keyboard.properties(ControlType = 'Position')
+keyboard.properties(Speed=4, ControlType='Position')
 
-# Add a pose sensor that exports the current location and orientation
-# of the robot in the world frame
-# Check here the other available actuators:
-# http://www.openrobots.org/morse/doc/stable/components_library.html#sensors
+# #############################################################################
 #
-# 'morse add sensor <name> morse_ws' can help you with the creation of a custom
-# sensor.
+# Sensors
+#
+# #############################################################################
+
+# Pose
 pose = Pose()
 robot.append(pose)
+pose.add_stream('ros')
+
+# Hokuyo Sensor
+# (https://www.hokuyo-aut.jp/02sensor/07scanner/urg_04lx_ug01.html)
+# 2cm to 5.6m | 240d |
+# Accuracy ±30mm (6cm<o<1m)
+# Accuracy 0.3% (1m<o<4.095m)
+hokuyo = Hokuyo()
+hokuyo.translate(0, 0, 1)
+hokuyo.rotate(0, 0, pi / 2)
+
+hokuyo.properties(laser_range=5.6)
+hokuyo.properties(resolution=0.36)
+hokuyo.properties(scan_window=240.0)
+# hokuyo.properties(Visible_arc=True)   # slows down simulation
+
+hokuyo.add_stream('ros')
+robot.append(hokuyo)
+
+# # Imu
+imu = IMU()
+imu.translate(0, 0, 0)
+imu.rotate(0, 0, 0)
+imu.add_stream('ros')
+robot.append(imu)
+
 
 # To ease development and debugging, we add a socket interface to our robot.
 #
-# Check here: http://www.openrobots.org/morse/doc/stable/user/integration.html 
+# Check here: http://www.openrobots.org/morse/doc/stable/user/integration.html
 # the other available interfaces (like ROS, YARP...)
 robot.add_default_interface('socket')
 
 
 # set 'fastmode' to True to switch to wireframe mode
-env = Environment('sandbox', fastmode = False)
-env.set_camera_location([-18.0, -6.7, 10.8])
+project_path = '/home/ejalaa12/Projects/GuerledanDamScanning/'
+env_folder = 'morse_ws/environments/'
+file = 'outdoors_flat_empty.blend'
+env = Environment(project_path + env_folder + file, fastmode=False)
+env.set_camera_location([-36.0, -15, 30])
 env.set_camera_rotation([1.09, 0, -1.14])
 
+# Properties for magnetomer in IMU
+env.properties(longitude=1.26, latitude=43.26, altitude=130.0)
