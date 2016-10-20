72286430941bdd6df08085d6660fb161b80077c2
ejalaa12 <ejalaa12@gmail.com>
2016-10-20 09:03:18 +0200

    added flag to keep speed fixed during the teleoperation

--- ros_ws/src/control/src/key_to_twist.py
+++ ros_ws/src/control/src/key_to_twist.py
@@ -15,11 +15,12 @@ key_mapping = {
     'd': [0, -0.1],  # droite
     's': [-0.1, 0],  # arriere
     ' ': [0, 0],    # stop
+    'f': [1, 1]     # flag
 }
 
 
 def keys_cb(msg, twist_pub):
-    global speed, rot
+    global speed, rot, flag
     if len(msg.data) == 0 or msg.data not in key_mapping.keys():
         return  # nothing to do, unknown key
     vels = key_mapping[msg.data[0]]
@@ -27,6 +28,8 @@ def keys_cb(msg, twist_pub):
         # we reset the speed and rot
         speed = 0
         rot = 0
+    elif msg.data == 'f':
+        flag = not flag
     else:
         speed += vels[0]
         rot += vels[1]
@@ -67,8 +70,10 @@ if __name__ == '__main__':
     twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     rospy.Subscriber('keys', String, keys_cb, twist_pub)
     rate = rospy.Rate(5)
+    flag = False
     while not rospy.is_shutdown():
-        speed = speed - 0.1 * sign(speed)
+        if not flag:
+            speed = speed - 0.1 * sign(speed)
         rot = rot - 0.1 * sign(rot)
         t = Twist()
         t.linear.x = speed
